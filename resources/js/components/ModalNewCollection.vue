926a65f671232d9f02163c6f9afdc33d14186fa3
Bryan H <34315804+bryann03@users.noreply.github.com>
2021-03-01 17:24:54 +0100

    add style modal new collection

--- resources/js/components/ModalNewCollection.vue
+++ resources/js/components/ModalNewCollection.vue
@@ -1,32 +1,38 @@
 <template>
-  <transition name="modal">
-    <div class="modal-mask">
-      <div class="modal-wrapper">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h3> New Collection </h3>
-          </div>
-
-          <div class="modal-body">
-            <form action="">
-                <div class="form-controller">
-                    <input type="text" name="" id="" placeholder="Name" class="form-control">
+    <transition name="modal">
+        <div class="modal-mask modal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">New collection</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="$emit('close')">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                <input type="text" class="form-control" id="recipient-name">
+                            </div>
+                            <div class="form-group">
+                                <label for="message-text" class="col-form-label">Message:</label>
+                                <textarea class="form-control" id="message-text"></textarea>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <slot name="footer">
+                            default footer
+                            <button class="btn btn-danger btn-block" @click="$emit('close')">
+                                Cancel
+                            </button>
+                        </slot>
+                    </div>
                 </div>
-            </form>
-          </div>
-
-          <div class="modal-footer">
-            <slot name="footer">
-              default footer
-              <button class="btn btn-danger btn-block" @click="$emit('close')">
-                Cancel
-              </button>
-            </slot>
-          </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </transition>
+    </transition>
 </template>
 
 <style lang="css">
@@ -40,22 +46,8 @@
     background-color: rgba(0, 0, 0, 0.5);
     display: table;
     transition: opacity 0.3s ease;
-}
-
-.modal-wrapper {
-    display: table-cell;
-    vertical-align: middle;
-}
-
-.modal-container {
-    width: 300px;
-    margin: 0px auto;
-    padding: 20px 30px;
-    background-color: #fff;
-    border-radius: 2px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-    transition: all 0.3s ease;
-    font-family: Helvetica, Arial, sans-serif;
+    justify-content:center;
+    align-items:center;
 }
 
 .modal-header h3 {
@@ -70,15 +62,6 @@
 .modal-default-button {
     float: right;
 }
-
-/*
-   * The following styles are auto-applied to elements with
-   * transition="modal" when their visibility is toggled
-   * by Vue.js.
-   *
-   * You can easily play with the modal transition by editing
-   * these styles.
-   */
 
 .modal-enter {
     opacity: 0;
